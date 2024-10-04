123032e9cb6d123094123f328f9652cf4c64a25d
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-10-04 11:25:13 +0530

    Added:Welcome Message

---
+++ src/com/bl/javastreams/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bl.javastreams.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book program");
+    }
+}
